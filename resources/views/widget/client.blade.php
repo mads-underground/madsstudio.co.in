cbdb6dc142a4b128907b012511b810fe6465df07
deepakbelbase <deepakbelbase.trancis@gmail.com>
2018-05-20 20:39:33 +0530

    Added: New Icons for Technology Used

--- resources/views/widget/client.blade.php
+++ resources/views/widget/client.blade.php
@@ -6,28 +6,19 @@
             <div class="clients">
               <ul class="slides text-center">
                 <li>
-                  <img src="{{ asset('assets/frontend/images/brands/1.png') }}" alt="">
+                  <img src="{{ asset('assets/frontend/images/brands/1.png') }}" alt="Laravel" title="laravel">
                 </li>
                 <li>
-                  <img src="{{ asset('assets/frontend/images/brands/2.png') }}" alt="">
+                  <img src="{{ asset('assets/frontend/images/brands/2.png') }}" alt="jQuery" title="jQuery">
                 </li>
                 <li>
-                  <img src="{{ asset('assets/frontend/images/brands/3.png') }}" alt="">
+                  <img src="{{ asset('assets/frontend/images/brands/3.png') }}" alt="node.js" title="node.js">
                 </li>
                 <li>
-                  <img src="{{ asset('assets/frontend/images/brands/4.png') }}" alt="">
+                  <img src="{{ asset('assets/frontend/images/brands/4.png') }}" alt="socket.io" title="socket.io">
                 </li>
                 <li>
-                  <img src="{{ asset('assets/frontend/images/brands/1.png') }}" alt="">
-                </li>
-                <li>
-                  <img src="{{ asset('assets/frontend/images/brands/2.png') }}" alt="">
-                </li>
-                <li>
-                  <img src="{{ asset('assets/frontend/images/brands/3.png') }}" alt="">
-                </li>
-                <li>
-                  <img src="{{ asset('assets/frontend/images/brands/4.png') }}" alt="">
+                  <img src="{{ asset('assets/frontend/images/brands/5.png') }}" alt="Voyager" title="Voyager">
                 </li>
               </ul>
             </div>
