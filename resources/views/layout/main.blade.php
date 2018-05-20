4d7fa9a60fc5326670530380569c0560a8c522c2
deepakbelbase <deepakbelbase.trancis@gmail.com>
2018-05-20 15:51:12 +0530

    Added: Index Page

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,172 @@
+  <!DOCTYPE html>
+  <html lang="en">
+
+
+  <!-- Mirrored from uxgoody.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 24 Feb 2018 05:57:00 GMT -->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <title>{{(isset($title)?$title:'404 Not Found')}} - {{ setting('site.title') }}</title>
+    <meta name="description" content="{{setting('site.description')}}">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="images/favicon.png">
+    <link rel="icon" href="{{ setting('site.favicon') }}_" type="image/png">
+    <link rel="shortcut icon" href="{{ setting('site.favicon') }}" type="image/x-icon">
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,600" rel="stylesheet">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/animate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/flexslider.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/lightbox.min.css') }}">
+      @yield('header-script')
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/responsive.css') }}">
+
+ 
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+
+
+  </head>
+  <body>
+
+    <!-- header navigation -->
+    <header id="navigation">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-2">
+            <a href="index.php"><img src="{{ setting('site.logo') }}" alt="Logo" class="main-nav-logo"></a>
+            <span class="ti-menu nav-btn-open hidden-md hidden-lg"></span>
+          </div>
+          <div class="col-md-8 text-center">
+            <ul class="main-nav">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#}}">Services</a></li>
+              <li><a href="#">Portfolio</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
+          <div class="col-md-2">
+            <a href="#" class="login-btn btn pull-right">Blog</a>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- header navigation end -->
+      @yield('content')    
+    <!-- footer -->
+    <footer id="footer" class="padding-tb50">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-md-offset-0 col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1">
+            <div class="col-md-4 padding-tb50">
+              <img src="{{ setting('site.flogo') }}" class="footerlogo" alt="">
+              <p class="footer-info-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Volu ptaonsec tetur adiu ptaonsec tetur adipisicing
+                elit. Volu ptatu pis icing elit. Vditiis.</p>
+              <p class="copyright">&copy; 2016 The Buzline theme</p>
+            </div>
+
+            <div class="col-md-2 padding-tb50">
+              <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Terms</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+              </ul>
+            </div>
+
+            <div class="col-md-2 padding-tb50">
+              <ul>
+                <li><a href="#">Team</a></li>
+                <li><a href="#">Comapny</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Log in</a></li>
+                <li><a href="#">Register</a></li>
+              </ul>
+            </div>
+
+            <div class="col-md-4 padding-tb50">
+              <div class="contact-details">
+                <div>
+                  <span class="glyphicon glyphicon-map-marker"></span>
+                  <p>
+                    164/9 Orchard St,New York,<br> NY 10002, United States.
+                  </p>
+                </div>
+
+                <div>
+                  <span class="glyphicon glyphicon-envelope"></span>
+                  <a href="#">{{ 'contact@buzline.com' }}</a>
+                </div>
+
+
+                <div>
+                  <span class="glyphicon glyphicon-earphone"></span>
+                  <p> +1 6485 658956</p>
+                </div>
+                <div class="socialnlinks">
+                  <a href="#"><span class="ti-facebook"></span></a>
+                  <a href="#"><span class="ti-instagram"></span></a>
+                  <a href="#"><span class="ti-twitter"></span></a>
+                  <a href="#"><span class="ti-linkedin"></span></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- footer end -->
+
+    <!-- sign in -->
+    <section id="signin_box">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-xs-12 signin_cont">
+            <p class="heading_login">LOGIN</p>
+            <div class="margin-t50">
+              <input type="email" name="email" placeholder="Enter Email" class="input_text">
+              <input type="password" name="mobile" placeholder="Enter Password" class="input_text">
+              <input type="submit" name="" value="SIGN IN" class="input_submit">
+              <a href="#" class="forgot_pass">Forgot Password?</a>
+            </div>
+            <span class="ti-close close_btn"></span>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- sign in end -->
+    <!-- MODERNIZR -->
+    <script src="{{ asset('assets/frontend/js/modernizr.js') }}"></script>
+
+    <!-- Scripts-->
+    <script src="{{ asset('assets/frontend/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/wow.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/jquery.flexslider.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/jquery.easing.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/SmoothScroll.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/parallax.min.js') }}"></script>
+    @yield('footer-script')
+    <script src="{{ asset('assets/frontend/js/lightbox.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/main.js') }}"></script>
+    <!-- Scripts End-->
+
+</body>
+
+<!-- Mirrored from uxgoody.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 24 Feb 2018 05:57:43 GMT -->
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,355 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout.main')
+@section('header-script')
+@stop
 
-        <title>Laravel</title>
+@section('content')
+ <!-- flexslider  -->
+  <section id="slider">
+    <div class="flexslider flexslide-container">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+      <ul class="slides">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <li style="background-image: url({{ asset('assets/frontend/images/slides/slide1.jpg') }}) ">
+          <div class="flex-caption fadeInUp animated container">
+            <p class="p-type-1">MULTIPURPOSE HTML5 RESPONSIVE THEME</p>
+            <h5 class="h5-type-1 margin-t20">Clean &amp; Modern Design</h5>
+            <div class="border-bar margin-t20 "></div>
+            <p class="p-type-2 margin-t20 text-justify">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in
+              some form, by injected humour, or randomised words which don't look even slightly believable. If you are going
+              to use a passage of Lorem Ipsum.</p>
+            <a href="#" class="btn login-btn">GET STARTED</a>
+          </div>
+        </li>
 
-            .full-height {
-                height: 100vh;
-            }
+        <li style="background-image: url({{ asset('assets/frontend/images/slides/slide2.jpg') }})">
+          <div class="flex-caption fadeInUp animated container">
+            <p class="p-type-1">MULTIPURPOSE HTML5 RESPONSIVE THEME</p>
+            <h5 class="h5-type-1 margin-t20">Clean &amp; Modern Design</h5>
+            <div class="border-bar margin-t20 "></div>
+            <p class="p-type-2 margin-t20 text-justify">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in
+              some form, by injected humour, or randomised words which don't look even slightly believable. If you are going
+              to use a passage of Lorem Ipsum.</p>
+            <a href="#" class="btn login-btn">GET STARTED</a>
+          </div>
+        </li>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        <li style="background-image: url({{ asset('assets/frontend/images/slides/slide3.jpg') }})">
+          <div class="flex-caption fadeInUp animated container">
+            <p class="p-type-1">MULTIPURPOSE HTML5 RESPONSIVE THEME</p>
+            <h5 class="h5-type-1 margin-t20">Clean &amp; Modern Design</h5>
+            <div class="border-bar margin-t20 "></div>
+            <p class="p-type-2 margin-t20 text-justify">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in
+              some form, by injected humour, or randomised words which don't look even slightly believable. If you are going
+              to use a passage of Lorem Ipsum.</p>
+            <a href="#" class="btn login-btn">GET STARTED</a>
+          </div>
+        </li>
 
-            .position-ref {
-                position: relative;
-            }
+      </ul>
+    </div>
+  </section>
+  <!-- flexslider end -->
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+  <!-- what we do  -->
+  <section id="whatwedo" class="padding-tb100">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <h4 class="h4-type2">What We Do</h4>
+          <div class="border-bar2 margin-t20 "></div>
+          <p class="p-type-3 color-grey margin-t20">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum ut alias ullam tempore nobis asperiores corporis
+            velit rem blanditiis! Ipsum facere, quae distinctio tempore, quas fugiat labore. Aliquid officiis, porro. tempore
+            nobis asperiores corporis velit rem blanditiis! Ipsum facere, quae distinctio tempore, quas fugiat labore. Aliquid
+            officiis, porro.
+          </p>
         </div>
-    </body>
-</html>
+      </div>
+
+    </div>
+  </section>
+  <!-- what we do  end-->
+
+  <!-- features -->
+  <section id="features" class="padding-tb100 bg-grey">
+    <div class="container">
+      <div class="row ">
+        <div class="col-md-4 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 text-center margin-tb10">
+          <span class="ti-paint-bucket icon-type1"></span>
+          <h4 class="h4-type1 margin-t10">Creative Design</h4>
+          <p class="p-type-4 color-grey margin-t10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum ut alias ullam tempore nobis asperiores quae
+            distinctio tempore.
+          </p>
+        </div>
+
+        <div class="col-md-4 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 text-center margin-tb10">
+          <span class="ti-desktop icon-type1"></span>
+          <h4 class="h4-type1 margin-t10">Responsive</h4>
+          <p class="p-type-4 color-grey margin-t10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum ut alias ullam tempore nobis asperiores quae
+            distinctio tempore.
+          </p>
+        </div>
+
+        <div class="col-md-4 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 text-center margin-tb10">
+          <span class="ti-pencil-alt icon-type1"></span>
+          <h4 class="h4-type1 margin-t10">Well Documented</h4>
+          <p class="p-type-4 color-grey margin-t10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum ut alias ullam tempore nobis asperiores quae
+            distinctio tempore.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- features end-->
+
+
+
+  <!-- who we are -->
+  <section id="whoweare">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 padding-tb100">
+          <h4 class="h4-type2 margin-t10">Who We Are</h4>
+          <div class="border-bar margin-t20 "></div>
+          <p class="p-type-5 color-grey margin-t20">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+            a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+          <a href="#" class="btn login-btn">GET STARTED</a>
+        </div>
+        <div class="col-md-6 col-xs-12 parallax-window" data-parallax="scroll" data-image-src="images/whoweare.jpg" data-speed="0.4">
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- who we are end-->
+
+  <!-- services -->
+  <section id="services" class="padding-tb100 bg-grey">
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-md-4 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 text-left margin-tb10">
+          <div class="col-md-3 col-xs-3 col-sm-3">
+            <span class="ti-star icon-type2"></span>
+          </div>
+          <div class="col-md-9 col-xs-9 col-sm-9">
+            <h4 class="h4-type1 margin-t10">Creative Design</h4>
+            <p class="p-type-4 color-grey margin-t10">Lorem ipsum dolor sit amet, consect etur adipis icing elit. Volupt atum ut alias ullam tempore nobis aspe.
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 text-left margin-tb10">
+          <div class="col-md-3 col-xs-3 col-sm-3">
+            <span class="ti-settings icon-type2"></span>
+          </div>
+          <div class="col-md-9 col-xs-9 col-sm-9">
+            <h4 class="h4-type1 margin-t10">Well Documented</h4>
+            <p class="p-type-4 color-grey margin-t10">Lorem ipsum dolor sit amet, consect etur adipis icing elit. Volupt atum ut alias ullam tempore nobis aspe.
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 text-left margin-tb10">
+          <div class="col-md-3 col-xs-3 col-sm-3">
+            <span class="ti-medall icon-type2"></span>
+          </div>
+          <div class="col-md-9 col-xs-9 col-sm-9">
+            <h4 class="h4-type1 margin-t10">Responsive Design</h4>
+            <p class="p-type-4 color-grey margin-t10">Lorem ipsum dolor sit amet, consect etur adipis icing elit. Volupt atum ut alias ullam tempore nobis aspe.
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 text-left margin-tb10">
+          <div class="col-md-3 col-xs-3 col-sm-3">
+            <span class="ti-announcement icon-type2"></span>
+          </div>
+          <div class="col-md-9 col-xs-9 col-sm-9">
+            <h4 class="h4-type1 margin-t10">SEO Friendly</h4>
+            <p class="p-type-4 color-grey margin-t10">Lorem ipsum dolor sit amet, consect etur adipis icing elit. Volupt atum ut alias ullam tempore nobis aspe.
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 text-left margin-tb10">
+          <div class="col-md-3 col-xs-3 col-sm-3">
+            <span class="ti-location-pin icon-type2"></span>
+          </div>
+          <div class="col-md-9 col-xs-9 col-sm-9">
+            <h4 class="h4-type1 margin-t10">Easy Customization</h4>
+            <p class="p-type-4 color-grey margin-t10">Lorem ipsum dolor sit amet, consect etur adipis icing elit. Volupt atum ut alias ullam tempore nobis aspe.
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1 text-left margin-tb10">
+          <div class="col-md-3 col-xs-3 col-sm-3">
+            <span class="ti-filter icon-type2"></span>
+          </div>
+          <div class="col-md-9 col-xs-9 col-sm-9">
+            <h4 class="h4-type1 margin-t10">Clean and Modern</h4>
+            <p class="p-type-4 color-grey margin-t10">Lorem ipsum dolor sit amet, consect etur adipis icing elit. Volupt atum ut alias ullam tempore nobis aspe.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- services End -->
+
+  <!-- portfolio -->
+  <section id="portfolio" class="padding-tb100">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-12">
+          <h4 class="h4-type2 text-center">Portfolio</h4>
+          <div class="border-bar2 margin-t20"></div>
+        </div>
+      </div>
+
+      <div class="row margin-t50">
+
+        <div class="col-md-4 margin-t30">
+          <div class="portfolio-item">
+            <a href="images/portfolio/item1.jpg" data-lightbox="portfolio">
+              <img src="{{ asset('assets/frontend/images/portfolio/item1.jpg') }}" alt="">
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-4 margin-t30">
+          <div class="portfolio-item">
+            <a href="images/portfolio/item2.jpg" data-lightbox="portfolio">
+              <img src="{{ asset('assets/frontend/images/portfolio/item2.jpg') }}" alt="">
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-4 margin-t30">
+          <div class="portfolio-item">
+            <a href="images/portfolio/item3.jpg" data-lightbox="portfolio">
+              <img src="{{ asset('assets/frontend/images/portfolio/item3.jpg') }}" alt="">
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-4 margin-t30">
+          <div class="portfolio-item">
+            <a href="images/portfolio/item47.jpg" data-lightbox="portfolio">
+              <img src="{{ asset('assets/frontend/images/portfolio/item47.jpg') }}" alt="">
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-4 margin-t30">
+          <div class="portfolio-item">
+            <a href="images/portfolio/item5.jpg" data-lightbox="portfolio">
+              <img src="{{ asset('assets/frontend/images/portfolio/item5.jpg') }}" alt="">
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-4 margin-t30">
+          <div class="portfolio-item">
+            <a href="images/portfolio/item6.jpg" data-lightbox="portfolio">
+              <img src="{{ asset('assets/frontend/images/portfolio/item6.jpg') }}" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+  <!-- portfolio end-->
+
+  <!-- testimonial -->
+  <section id="testimonial" class="padding-tb100 parallax-testimonial" data-parallax="scroll" data-image-src="{{ asset('assets/frontend/images/parallax-back.jpg') }}"
+    data-speed="0.4">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="testimonial">
+            <ul class="slides text-center">
+              <li>
+                <p class="testimonial-text">Nulla nec nisi viverra, facilisis nisl in, pulvinar metus. Mauris sit amet ligula blandit, efficitur magna
+                  et, hendrerit nisi. Suspendisse dapibus venenatis nunc.</p>
+                <div class="border-bar3"></div>
+                <h5>John Doe</h5>
+                <h6>CEO of Design Company</h6>
+              </li>
+              <li>
+                <p class="testimonial-text">Nulla nec nisi viverra, facilisis nisl in, pulvinar metus. Mauris sit amet ligula blandit, efficitur magna
+                  et, hendrerit nisi. Suspendisse dapibus venenatis nunc.</p>
+                <div class="border-bar3"></div>
+                <h5>John Doe</h5>
+                <h6>CEO of Design Company</h6>
+              </li>
+              <li>
+                <p class="testimonial-text">Nulla nec nisi viverra, facilisis nisl in, pulvinar metus. Mauris sit amet ligula blandit, efficitur magna
+                  et, hendrerit nisi. Suspendisse dapibus venenatis nunc.</p>
+                <div class="border-bar3"></div>
+                <h5>John Doe</h5>
+                <h6>CEO of Design Company</h6>
+              </li>
+              <li>
+                <p class="testimonial-text">Nulla nec nisi viverra, facilisis nisl in, pulvinar metus. Mauris sit amet ligula blandit, efficitur magna
+                  et, hendrerit nisi. Suspendisse dapibus venenatis nunc.</p>
+                <div class="border-bar3"></div>
+                <h5>John Doe</h5>
+                <h6>CEO of Design Company</h6>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    </section>
+    <!-- testimonial end-->
+
+    <!-- clients -->
+    <section id="clients" class="padding-tb50">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="clients">
+              <ul class="slides text-center">
+                <li>
+                  <img src="{{ asset('assets/frontend/images/brands/1.png') }}" alt="">
+                </li>
+                <li>
+                  <img src="{{ asset('assets/frontend/images/brands/2.png') }}" alt="">
+                </li>
+                <li>
+                  <img src="{{ asset('assets/frontend/images/brands/3.png') }}" alt="">
+                </li>
+                <li>
+                  <img src="{{ asset('assets/frontend/images/brands/4.png') }}" alt="">
+                </li>
+                <li>
+                  <img src="{{ asset('assets/frontend/images/brands/1.png') }}" alt="">
+                </li>
+                <li>
+                  <img src="{{ asset('assets/frontend/images/brands/2.png') }}" alt="">
+                </li>
+                <li>
+                  <img src="{{ asset('assets/frontend/images/brands/3.png') }}" alt="">
+                </li>
+                <li>
+                  <img src="{{ asset('assets/frontend/images/brands/4.png') }}" alt="">
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- clients end -->
+
+@stop
+
+@section('footer-script')
+@stop
