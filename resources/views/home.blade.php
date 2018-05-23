6ccb7da3a08975f10f63b80caf661116f0a1309f
deepakbelbase <deepakbelbase.trancis@gmail.com>
2018-05-23 15:22:06 +0530

    "Added: Portfolio Image with Link"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -164,71 +164,7 @@
   </section>
   <!-- services End -->
 
-  <!-- portfolio -->
-  <section id="portfolio" class="padding-tb100">
-    <div class="container">
-
-      <div class="row">
-        <div class="col-md-12">
-          <h4 class="h4-type2 text-center">Portfolio</h4>
-          <div class="border-bar2 margin-t20"></div>
-        </div>
-      </div>
-
-      <div class="row margin-t50">
-
-        <div class="col-md-4 margin-t30">
-          <div class="portfolio-item">
-            <a href="images/portfolio/item1.jpg" data-lightbox="portfolio">
-              <img src="{{ asset('assets/frontend/images/portfolio/item1.jpg') }}" alt="">
-            </a>
-          </div>
-        </div>
-
-        <div class="col-md-4 margin-t30">
-          <div class="portfolio-item">
-            <a href="images/portfolio/item2.jpg" data-lightbox="portfolio">
-              <img src="{{ asset('assets/frontend/images/portfolio/item2.jpg') }}" alt="">
-            </a>
-          </div>
-        </div>
-
-        <div class="col-md-4 margin-t30">
-          <div class="portfolio-item">
-            <a href="images/portfolio/item3.jpg" data-lightbox="portfolio">
-              <img src="{{ asset('assets/frontend/images/portfolio/item3.jpg') }}" alt="">
-            </a>
-          </div>
-        </div>
-
-        <div class="col-md-4 margin-t30">
-          <div class="portfolio-item">
-            <a href="images/portfolio/item47.jpg" data-lightbox="portfolio">
-              <img src="{{ asset('assets/frontend/images/portfolio/item47.jpg') }}" alt="">
-            </a>
-          </div>
-        </div>
-
-        <div class="col-md-4 margin-t30">
-          <div class="portfolio-item">
-            <a href="images/portfolio/item5.jpg" data-lightbox="portfolio">
-              <img src="{{ asset('assets/frontend/images/portfolio/item5.jpg') }}" alt="">
-            </a>
-          </div>
-        </div>
-
-        <div class="col-md-4 margin-t30">
-          <div class="portfolio-item">
-            <a href="images/portfolio/item6.jpg" data-lightbox="portfolio">
-              <img src="{{ asset('assets/frontend/images/portfolio/item6.jpg') }}" alt="">
-            </a>
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </section>
-  <!-- portfolio end-->
+  @include('widget.portfolio')
 
   <!-- testimonial -->
   <section id="testimonial" class="padding-tb100 parallax-testimonial" data-parallax="scroll" data-image-src="{{ asset('assets/frontend/images/parallax-back.jpg') }}"
---
+++ resources/views/widget/portfolio.blade.php
@@ -0,0 +1,65 @@
+<!-- portfolio -->
+  <section id="portfolio" class="padding-tb100">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-12">
+          <h4 class="h4-type2 text-center">Portfolio</h4>
+          <div class="border-bar2 margin-t20"></div>
+        </div>
+      </div>
+
+      <div class="row margin-t50">
+
+        <div class="col-md-4 margin-t30">
+          <div class="portfolio-item">
+            <a href="http://www.usafashionslab.com/" target="_blank">
+              <img src="{{ asset('assets/frontend/images/portfolio/item1.png') }}" alt="">
+            </a>
+          </div>
+        </div>
+
+        {{-- <div class="col-md-4 margin-t30">
+          <div class="portfolio-item">
+            <a href="images/portfolio/item2.jpg" data-lightbox="portfolio">
+              <img src="{{ asset('assets/frontend/images/portfolio/item2.jpg') }}" alt="">
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-4 margin-t30">
+          <div class="portfolio-item">
+            <a href="images/portfolio/item3.jpg" data-lightbox="portfolio">
+              <img src="{{ asset('assets/frontend/images/portfolio/item3.jpg') }}" alt="">
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-4 margin-t30">
+          <div class="portfolio-item">
+            <a href="images/portfolio/item47.jpg" data-lightbox="portfolio">
+              <img src="{{ asset('assets/frontend/images/portfolio/item47.jpg') }}" alt="">
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-4 margin-t30">
+          <div class="portfolio-item">
+            <a href="images/portfolio/item5.jpg" data-lightbox="portfolio">
+              <img src="{{ asset('assets/frontend/images/portfolio/item5.jpg') }}" alt="">
+            </a>
+          </div>
+        </div>
+
+        <div class="col-md-4 margin-t30">
+          <div class="portfolio-item">
+            <a href="images/portfolio/item6.jpg" data-lightbox="portfolio">
+              <img src="{{ asset('assets/frontend/images/portfolio/item6.jpg') }}" alt="">
+            </a>
+          </div>
+        </div>
+      </div> --}}
+
+    </div>
+  </section>
+  <!-- portfolio end
